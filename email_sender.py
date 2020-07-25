544af16651418cae856a665044c69d4f1287a4e3
Usha Kannan <42096292+usha010699@users.noreply.github.com>
2020-07-25 13:10:09 +0530

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -2,8 +2,8 @@ import smtplib
 from email.message import EmailMessage
 
 email =EmailMessage()
-email['from']='sha natashah'
-email['to']='nishakannan1705@gmail.com'
+email['from']='email name'
+email['to']='xxxxxxxx@gmail.com'
 email['subject']='you are placed'
 
 email.set_content('i m a developer')
@@ -11,6 +11,6 @@ email.set_content('i m a developer')
 with smtplib.SMTP(host='smtp.gmail.com',port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('ushanatashah@gmail.com','usha kannan')
+    smtp.login('ur email id@gmail.com','ur email id pass')
     smtp.send_message(email)
     print('all good boss!!:)')
